7d1ff0b37c36e6fcc49b63d27aa97a78d0ce5481
BBQ <barbq@mail2000.com.tw>
2022-10-21 21:58:57 +0800

    change theme to sphinx_rtd_theme

--- source/conf.py
+++ source/conf.py
@@ -24,8 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'nature'
+# html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
 
-html_theme = 'sphinx_bernard_theme'
-html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
