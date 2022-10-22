1551a496aa21500fc34d1f16439ddd03de45995a
BBQ <barbq@mail2000.com.tw>
2022-10-22 17:00:43 +0800

    add markdown related

--- source/conf.py
+++ source/conf.py
@@ -10,11 +10,27 @@ project = 'bbqtest'
 copyright = '2022, bbq'
 author = 'bbq'
 release = '0.1'
+version = '0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'recommonmark',
+    'sphinx_markdown_tables',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,4 +42,21 @@ exclude_patterns = []
 
 # html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
+# -- Options for HTML output
+html_theme = 'sphinx_rtd_theme'
 
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# -- Options for PDF output
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
+
+source_parsers = {
+    '.md': 'recommonmark.parser.CommonMarkParser',
+}
+
+source_suffix = ['.rst', '.md']
